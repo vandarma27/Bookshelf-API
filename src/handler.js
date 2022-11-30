eb53cf1c99e8ba046313ef0d42ddb326c54913ed
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-30 20:24:59 +0700

    Update handler.js
    membuat handler get book by id

--- src/handler.js
+++ src/handler.js
@@ -81,4 +81,26 @@ const getAllBooksHandler = () => ({
   },
 });
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
